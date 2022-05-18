09cc894ede5dd5ac54588ad41fc5b73f7e6b7bb6
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-18 23:45:03 +0200

    Dice game with Java

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
+import java.util.Random;
+
 public class Main{
 
     public static void main(String[] args) {
+
+        Random rand = new Random();
+        int max = 6;
         
-        System.out.println("Hello world");
 
-        int numeroRandom = 5;
+        int user = rand.nextInt(max);
+        int computer = rand.nextInt(max);
 
-
-        System.out.println(numeroRandom);
+        if (user > computer) {
+            System.out.println("User: " + user);
+            System.out.println("Computer: " + computer);
+            System.out.println("HAI VINTO! :D");
+        } else if(computer > user) {
+            System.out.println("User: " + user);
+            System.out.println("Computer: " + computer);
+            System.out.println("HAI PERSO! :(");
+        } else {
+            System.out.println("User: " + user);
+            System.out.println("Computer: " + computer);
+            System.out.println("PAREGGIO! :/");
+        }
         
     }
 }
