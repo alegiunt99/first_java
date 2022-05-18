16471f3854ba9f843874674b7558f63595d9e660
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-18 23:15:35 +0200

    First Java project

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+
+        int numeroRandom = 5;
+
+
+        System.out.println(numeroRandom);
+        
+    }
+}
